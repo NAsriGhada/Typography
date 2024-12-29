94d2906c3da607896ac51f95f95fe62e1c53a01f
Ghada Nasri <92127587+NAsriGhada@users.noreply.github.com>
2024-12-29 20:33:50 +0100

    Add files via upload

---
+++ Typography.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { generateClasses } from './typographyHelpers';
+import type { TypographyProps } from './index';
+const Typography: React.FC<TypographyProps> = ({
+  as: Tag = 'p',
+  text,
+  ...props
+}) => {
+  const classes = generateClasses({ ...props });
+
+  console.log(classes);
+  return <Tag className={`${classes}`}>{text}</Tag>;
+};
+
+export default Typography;
